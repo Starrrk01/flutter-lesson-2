b932964e94cb9d71a079e5101c2d724036fdef96
jokermt2357 <jokermt2357@gmail.com>
2026-03-07 02:00:34 +0600

    Upload lesson2 state management

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:lesson_flutter_1/model/user.dart';
+import 'package:lesson_flutter_1/user/presentation/login.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // Application name
+      title: 'Flutter Hello World',
+      // Application theme data, you can set the colors for the application as
+      // you want
+      theme: ThemeData(
+        // useMaterial3: false,
+        primarySwatch: Colors.blue,
+      ),
+      // A widget which will be started on application startup
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final String title;
+  const MyHomePage({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    User user = User(firstName: "Adiltet", lastName: "Adiletov");
+    return Login();
+  }
+}
---
+++ lib/user/presentation/login.dart
@@ -0,0 +1,97 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  String defaultLogin = "login";
+  String defaultPassword = "password";
+  Login({super.key});
+  @override
+  LoginState createState() => LoginState();
+}
+
+class LoginState extends State<Login> {
+  String login = "";
+  String password = "";
+  bool viewBox = false;
+  TextEditingController loginController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  @override
+  void initState() {
+    login = widget.defaultLogin;
+    password = widget.defaultPassword;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Login"),
+      ),
+      body: Form(
+        child: Column(
+          children: [
+            Container(
+              margin: const EdgeInsets.only(left: 8, right: 8, bottom: 8),
+              child: TextFormField(
+                controller: loginController,
+                decoration: const InputDecoration(
+                  label: Text("Username"),
+                ),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(left: 8, right: 8, bottom: 8),
+              child: TextFormField(
+                controller: passwordController,
+                obscureText: true,
+                decoration: const InputDecoration(label: Text("Password")),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 8),
+              child: ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    viewBox = true;
+                    login = loginController.text;
+                    password = passwordController.text;
+                  });
+                },
+                style: ButtonStyle(
+                    backgroundColor:
+                        WidgetStateProperty.all<Color>(Colors.blue)),
+                child: const Text("Sign in"),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.all(8),
+              child: Row(
+                children: [
+                  Container(
+                    margin: const EdgeInsets.only(right: 8),
+                    child: Text(login),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(left: 8),
+                    child: Text(password),
+                  ),
+                ],
+              ),
+            ),
+            viewBox
+                ? Container(
+                    width: 50,
+                    height: 50,
+                    decoration: BoxDecoration(
+                        color: Colors.amber,
+                        border: Border.all(color: Colors.black)),
+                  )
+                : const SizedBox()
+          ],
+        ),
+      ),
+    );
+  }
+}
